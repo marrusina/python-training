30ed8bde0e593b4a07b0c970c47a8959e12512f5
marina rusina <rusina@reksoft.ru>
2021-02-22 12:00:03 +0300

    test_ten

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -5,14 +5,17 @@ class NavigationHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("searchstring")) > 0):
+            wd.get("http://localhost/addressbook/")
 
     def return_to_groups_page(self):
         # return to groups page
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("group page").click()
 
