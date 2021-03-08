c5f8d9b18dee7d310c2272e9c7a4045c08e09bc4
marina rusina <rusina@reksoft.ru>
2021-03-08 16:22:52 +0300

    test_fourteen

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,6 +7,7 @@ from random import randint
 import time
 from fixture.navigation import NavigationHelper
 from model.contacts import Contacts
+import re
 class ContactHelper:
     def __init__(self, app):
         self.app = app
@@ -74,13 +75,11 @@ class ContactHelper:
         wd = self.app.wd
         # select first contact
         wd.find_element_by_name("selected[]").click()
-        #wd.find_element_by_xpath("//img[@alt='Edit']").click()
 
     def select_contact_by_index(self, index):
         wd = self.app.wd
         # select first contact
         wd.find_elements_by_name("selected[]")[index].click()
-        #wd.find_element_by_xpath("//img[@alt='Edit']").click()
 
     def edit_first_contact(self):
         wd = self.app.wd
@@ -89,6 +88,14 @@ class ContactHelper:
     def edit_contact_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
+
+    def open_first_contact_view(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Details']").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_xpath("//img[@alt='Details']")[index].click()
 
     def count(self):
         wd = self.app.wd
@@ -123,10 +130,44 @@ class ContactHelper:
             NavigationHelper.open_home_page(self)
             self.contact_cache = []
             for element in wd.find_elements_by_xpath("//tr[@name='entry']"):
+                cells = element.find_elements_by_tag_name("td")
                 contact_id = element.find_element_by_name("selected[]").get_attribute("value")
                 lastname = element.find_element_by_xpath("./td[2]").text
                 firstname = element.find_element_by_xpath("./td[3]").text
+                address = element.find_element_by_xpath("./td[4]").text
+                all_phones = cells[5].text
+                all_emails = cells[4].text
                 self.contact_cache.append(Contacts(id=contact_id,
                                         lastname=lastname,
-                                        firstname=firstname))
+                                        firstname=firstname, address=address, all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return self.contact_cache
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        NavigationHelper.open_home_page(self)
+        self.select_contact_by_index(index)
+        self.edit_contact_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        return Contacts(firstname=firstname, lastname=lastname, address=address, id=id, home=home, mobile=mobile, phone2=phone2,
+                        email=email, email2=email2, email3=email3)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        NavigationHelper.open_home_page(self)
+        self.select_contact_by_index(index)
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contacts(home=home, mobile=mobile, phone2=phone2)
+
--- model/contacts.py
+++ model/contacts.py
@@ -1,8 +1,10 @@
 from sys import maxsize
 class Contacts:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home=None, mobile=None, email=None, bday=None, bmonth=None,
-                            byear=None, address2=None, phone2=None, notes=None, id=None):
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None,
+                 home=None, mobile=None, email=None, email2=None, email3=None, bday=None, bmonth=None,
+                            byear=None, address2=None, phone2=None, notes=None, id=None,
+                 all_phones_from_home_page=None, all_emails_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -13,6 +15,8 @@ class Contacts:
         self.home = home
         self.mobile = mobile
         self.email = email
+        self.email2 = email2
+        self.email3 = email3
         self.bday = bday
         self.bmonth = bmonth
         self.byear = byear
@@ -20,6 +24,8 @@ class Contacts:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
