a38ebf1eb96b7ce0318506f19b49176109ca44c1
marina rusina <rusina@reksoft.ru>
2021-03-15 16:43:47 +0300

    test_eighteen

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
-import pytest
-import random
-import string
+
 from model.group import Group
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
-    Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-            for i in range(5)
-]
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, json_groups):
+    group = json_groups
     app.navigation.open_groups_page()
     old_groups = app.group.get_group_list()
     app.group.create(group)
